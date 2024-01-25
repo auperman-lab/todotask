47687490e98f94f837ba61729aa894bd1810d27b
auperman <naauperman@gmail.com>
2024-01-25 22:14:43 +0200

    added information from firebase

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../model/todo.dart';
 import '../constants/colors.dart';
 
 class ToDoItem extends StatefulWidget {
-  final ToDo todo;
-  final Function(ToDo) onDone;
+  final String title;
+  final String description;
+  final DateTime expirationDate;
+  final bool isDone;
+  final String tid;
+  final Function(String) onDone;
   final Function(String) onDeleteItem;
-  final Function(ToDo) onDetails;
+  final Function(String) onDetails;
 
   const ToDoItem({
     Key? key,
-    required this.todo,
+    required this.title,
+    required this.description,
+    required this.expirationDate,
+    required this.tid,
+    required this.isDone,
     required this.onDone,
     required this.onDeleteItem,
     required this.onDetails,
@@ -26,19 +33,19 @@ class _ToDoItemState extends State<ToDoItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 20),
+      margin: const EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {
-          widget.onDetails(widget.todo);
+          widget.onDetails(widget.tid);
         },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Container(
-          padding: EdgeInsets.all(0),
-          margin: EdgeInsets.symmetric(vertical: 12),
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 12),
           height: 35,
           width: 35,
           decoration: BoxDecoration(
@@ -49,32 +56,32 @@ class _ToDoItemState extends State<ToDoItem> {
             color: Colors.blue,
             iconSize: 18,
             icon: Icon(
-              widget.todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+              widget.isDone ? Icons.check_box : Icons.check_box_outline_blank,
             ),
             onPressed: () {
-              widget.onDone(widget.todo);
+              widget.onDone(widget.tid);
               setState(() {}); // Rebuild the widget when the state changes
             },
           ),
         ),
         title: Text(
-          widget.todo.todoText!,
+          widget.title,
           style: TextStyle(
             fontSize: 16,
             color: tdBlack,
-            decoration: widget.todo.isDone ? TextDecoration.lineThrough : null,
+            decoration: widget.isDone ? TextDecoration.lineThrough : null,
           ),
         ),
         subtitle: Text(
-          'Expiration Date: ${DateFormat('yyyy-MM-dd').format(widget.todo.expirationDate!)}',
-          style: TextStyle(
+          'Expiration Date: ${DateFormat('yyyy-MM-dd').format(widget.expirationDate)}',
+          style: const TextStyle(
             fontSize: 14,
             color: tdGrey,
           ),
         ),
         trailing: Container(
-          padding: EdgeInsets.all(0),
-          margin: EdgeInsets.symmetric(vertical: 12),
+          margin: const EdgeInsets.symmetric(vertical: 12),
+          padding: const EdgeInsets.all(0),
           height: 35,
           width: 35,
           decoration: BoxDecoration(
@@ -84,9 +91,9 @@ class _ToDoItemState extends State<ToDoItem> {
           child: IconButton(
             color: Colors.white,
             iconSize: 18,
-            icon: Icon(Icons.delete),
+            icon: const Icon(Icons.delete),
             onPressed: () {
-              widget.onDeleteItem(widget.todo.id!);
+              widget.onDeleteItem(widget.tid);
             },
           ),
         ),
